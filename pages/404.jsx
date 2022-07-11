7649e10a69f5dc1f6ae3e12b6d0844ce8e420c86
Jeel Gajera <83470656+JeelGajera@users.noreply.github.com>
2022-07-11 18:48:04 +0530

    fix: class sysntax

--- pages/404.jsx
+++ pages/404.jsx
@@ -8,15 +8,15 @@ function NotFound() {
     return (
         <>
             <Layout title="Ooops... | Go back to earth 🤖">
-                <div class="justify-center">
-                    <center class="mt-24 m-auto">
+                <div className="justify-center">
+                    <center className="mt-24 m-auto">
                         <div className="animate-bounce">
                             <Image src={imgPath} alt="404" width={200} height={200} />
                         </div>
-                        <div class=" tracking-widest">
-                            <span class="text-gray-500 mb-2 text-6xl block"><span>4  0  4</span></span>
+                        <div className=" tracking-widest">
+                            <span className="text-gray-500 mb-2 text-6xl block"><span>4  0  4</span></span>
                             <hr className="w-24 h-[2px] rounded-lg m-2 border-none bg-teal-500" />
-                            <span class="text-gray-500 text-xl">Sorry, We couldn't find what you are looking for!</span>
+                            <span className="text-gray-500 text-xl">Sorry, We couldn't find what you are looking for!</span>
                         </div>
                         <button className="px-2 h-8 mt-3 flex items-center justify-center shadow-xl text-[#f8b34b] hover:text-red-500 text-xl bg-red-500 bg-opacity-10 hover:bg-teal-400 hover:bg-opacity-10 backdrop-blur-md rounded-full active:translate-y-1"
                             onClick={() => window.history.back()}>
